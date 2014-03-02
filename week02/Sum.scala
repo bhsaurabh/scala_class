85903cdb40f69a52d34a030f571ef226112f337c
bhsaurabh <srbhatia10@gmail.com>
2014-03-02 10:05:07 +0530

    Used tail recursion

--- week02/Sum.scala
+++ week02/Sum.scala
@@ -1,9 +1,12 @@
 object Sum {
 	// higher  order function used
-	/* Compute sum of numbers from a to b */
-	def sum(f: Int => Int, a: Int, b: Int): Int = 
-		if (a > b)	0
-		else	f(a) + sum(f, a+1, b)
+	/* Compute sum of numbers from a to b -> Using tail recursion */
+	def sum(f: Int => Int, a: Int, b: Int): Int = { 
+		def loop(a: Int, acc: Int): Int = 
+			if (a > b)	acc
+			else	loop(a+1, acc+f(a))
+		loop(a, 0)		
+}
 
 	// unit testing...use anonymous functions 
 	def main(args: Array[String]) = {
